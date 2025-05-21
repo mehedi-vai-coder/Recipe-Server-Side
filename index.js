6c1ceec7ff8226adf21e839b77d428748d1c60c0
MD MEHEDI HASAN <md.mehedihasan8n@gmail.com>
2025-05-21 23:38:24 +0600

    Connected with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER_RECIPE}:${process.env.DB_PASS_RECIPE}@cluster0.tmx4o0d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {

5d5df8fff387a3599b79ff052a336f0d8ac6e641
MD MEHEDI HASAN <md.mehedihasan8n@gmail.com>
2025-05-22 02:57:06 +0600

    get and delete method done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
 const app = express();
@@ -23,6 +23,29 @@ async function run() {
   try {
    
     await client.connect();
+     const recipeCollection = client.db('RecipeDB').collection('recipe')
+
+     // send data to db/ Post 
+    app.post('/recipies', async (req, res) => {
+      const newRecipe = req.body;
+      const result = await recipeCollection.insertOne(newRecipe);
+      res.send(result);
+    })
+    // To get all the recipies
+     app.get('/recipies', async (req, res) => {
+      const result = await recipeCollection.find().toArray();
+      res.send(result);
+    });
+    // Delete the data from database
+      app.delete('/recipies/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await recipeCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+    // To confirm Connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
